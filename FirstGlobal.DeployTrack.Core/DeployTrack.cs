9b84ade22c455c42724a47e228a1f65dc7db199b
Nischal Panwala <npanwala@1stglobal.com>
2017-08-10 11:06:40 -0500

    changes from Nischal

--- FirstGlobal.DeployTrack.Core/DeployTrack.cs
+++ FirstGlobal.DeployTrack.Core/DeployTrack.cs
@@ -4,7 +4,6 @@ using System.Text;
 
 namespace FirstGlobal.DeployTrack.Core
 {
-    //Testing changes from Nischal
     public class DeployTrack
     {
         public string ModuleName { get; set; }
